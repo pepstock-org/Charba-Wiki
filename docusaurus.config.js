fa651369493042800b7a15b504c7e73bf0b40f1b
stock <stocki.nail@gmail.com>
2021-07-23 17:14:09 +0200

    changes version name, removing "unreleased"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ module.exports = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '4.1 unreleased',
+              label: '4.1',
             },
           },
         },
